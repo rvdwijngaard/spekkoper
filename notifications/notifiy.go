b77786cd63e6f2fff83c37082e5bc64666a7fccd
Ron van der Wijngaard <rvdwijngaard@innius.com>
2022-09-07 16:25:13 +0200

    feat: add real notifications via ntfy.sh

--- notifications/notifiy.go
+++ notifications/notifiy.go
@@ -2,10 +2,11 @@ package notifications
 
 import (
 	"context"
+	"net/http"
+	"strings"
 
 	"encore.app/spekkoper"
 	"encore.dev/pubsub"
-	"encore.dev/rlog"
 )
 
 var _ = pubsub.NewSubscription(
@@ -16,6 +17,12 @@ var _ = pubsub.NewSubscription(
 )
 
 func SendWelcomeEmail(ctx context.Context, event *spekkoper.NewQueryResultEvent) error {
-	rlog.Info("got a new ad", "ad", event.Advertisement)
-	return nil
+	req, _ := http.NewRequest("POST", "https://ntfy.sh/spekkoper",
+		strings.NewReader(`New advertisement. 🐶`))
+	req.Header.Set("Click", event.Advertisement.URL)
+	//req.Header.Set("Attach", "https://nest.com/view/yAxkasd.jpg")
+	//req.Header.Set("Actions", "http, Open door, https://api.nest.com/open/yAxkasd, clear=true")
+	req.Header.Set("Email", "ronvanderwijngaard@kliksafe.nl")
+	_, err := http.DefaultClient.Do(req)
+	return err
 }
